e13c6de8cef23e397e66e25099ae66e0f90605eb
Ivan <ivan.okhotnikov@outlook.com>
2021-11-08 13:02:49 +0000

    Reorganized to DataReader, ModelReader and Preprocessor classes. Tested

--- code/scripts/utils/readers.py
+++ code/scripts/utils/readers.py
@@ -2,149 +2,172 @@ import os
 import numpy as np
 import pandas as pd
 
-from scipy import stats
-from . import config as c
+from config import DATA_PATH, FEATURES, FEATURES_NO_TIME
 
 
-def read_all_raw_data(verbose=True):
-    final_df = pd.DataFrame()
-    units = []
-    for file in os.listdir(c.DATA_PATH):
-        if verbose:
-            print(f'Reading {file}')
-        try:
-            if file.endswith('.csv'):
-                current_df = pd.read_csv(
-                    os.path.join(c.DATA_PATH, file),
-                    usecols=c.FEATURES,
-                    on_bad_lines='skip',
-                    index_col=False,
-                    header=0,
-                )
-            elif file.endswith('.xlsx'):
-                current_df = pd.read_excel(
-                    os.path.join(c.DATA_PATH, file),
-                    usecols=c.FEATURES,
-                    index_col=False,
-                    header=0,
-                )
-        except ValueError:
+class DataReader:
+    def read_all_raw_data(self, verbose=True):
+        final_df = pd.DataFrame()
+        units = []
+        for file in os.listdir(os.path.join(DATA_PATH, 'raw')):
             if verbose:
-                print(f'{file} got a faulty header')
-            continue
-        current_df[c.FEATURES_NO_TIME] = current_df[c.FEATURES_NO_TIME].apply(
-            pd.to_numeric,
-            errors='coerce',
-            downcast='float',
-        )
-        name_list = file.split('-')
-        try:
-            unit = np.uint8(name_list[0][-2:])
-        except ValueError:
-            unit = np.uint8(name_list[0].split('_')[0][-2:])
-        units.append(unit)
-        current_df['ARMANI'] = 1 if name_list[0][3] == '2' else 0
-        current_df['UNIT'] = unit
-        current_df['TEST'] = np.uint8(units.count(unit))
-        current_df['STEP'] = current_df['STEP'].astype(
-            np.uint8,
-            errors='ignore',
-        )
-        current_df = current_df.dropna(axis=0)
-        final_df = pd.concat((final_df, current_df), ignore_index=True)
-    return final_df
+                print(f'Reading {file}')
+            try:
+                if file.endswith('.csv'):
+                    current_df = pd.read_csv(
+                        os.path.join(DATA_PATH, 'raw', file),
+                        usecols=FEATURES,
+                        on_bad_lines='skip',
+                        index_col=False,
+                        header=0,
+                    )
+                elif file.endswith('.xlsx'):
+                    current_df = pd.read_excel(
+                        os.path.join(DATA_PATH, 'raw', file),
+                        usecols=FEATURES,
+                        index_col=False,
+                        header=0,
+                    )
+            except ValueError:
+                if verbose:
+                    print(f'{file} got a faulty header')
+                continue
+            current_df[FEATURES_NO_TIME] = current_df[FEATURES_NO_TIME].apply(
+                pd.to_numeric,
+                errors='coerce',
+                downcast='float',
+            )
+            name_list = file.split('-')
+            try:
+                unit = np.uint8(name_list[0][-2:])
+            except ValueError:
+                unit = np.uint8(name_list[0].split('_')[0][-2:])
+            units.append(unit)
+            current_df['ARMANI'] = 1 if name_list[0][3] == '2' else 0
+            current_df['UNIT'] = unit
+            current_df['TEST'] = np.uint8(units.count(unit))
+            current_df['STEP'] = current_df['STEP'].astype(
+                np.uint8,
+                errors='ignore',
+            )
+            current_df = current_df.dropna(axis=0)
+            final_df = pd.concat((final_df, current_df), ignore_index=True)
+        return final_df
 
-
-def read_raw_unit_data(unit_id='HYD000091-R1_RAW'):
-    try:
-        unit_df = pd.read_csv(os.path.join(c.DATA_PATH, unit_id + '.csv'),
-                              usecols=c.FEATURES,
-                              index_col=False,
-                              header=0)
-    except:
+    def read_raw_unit_data(self, unit_id='HYD000091-R1_RAW'):
         try:
-            unit_df = pd.read_excel(os.path.join(c.DATA_PATH,
-                                                 unit_id + '.xlsx'),
-                                    usecols=c.FEATURES,
-                                    index_col=False,
-                                    header=0)
+            unit_df = pd.read_csv(os.path.join(DATA_PATH, 'raw',
+                                               unit_id + '.csv'),
+                                  usecols=FEATURES,
+                                  index_col=False,
+                                  header=0)
         except:
-            print(f'No {unit_id} file found')
-            return None
-    unit_df[c.FEATURES_NO_TIME] = unit_df[c.FEATURES_NO_TIME].apply(
-        pd.to_numeric, errors='coerce')
-    unit_df[c.FEATURES_NO_TIME] = unit_df[c.FEATURES_NO_TIME].astype(
-        np.float32)
-    unit_df = unit_df.dropna(axis=0)
-    return unit_df
+            try:
+                unit_df = pd.read_excel(os.path.join(DATA_PATH, 'raw',
+                                                     unit_id + '.xlsx'),
+                                        usecols=FEATURES,
+                                        index_col=False,
+                                        header=0)
+            except:
+                print(f'No {unit_id} file found')
+                return None
+        unit_df[FEATURES_NO_TIME] = unit_df[FEATURES_NO_TIME].apply(
+            pd.to_numeric, errors='coerce')
+        unit_df[FEATURES_NO_TIME] = unit_df[FEATURES_NO_TIME].astype(
+            np.float32)
+        unit_df = unit_df.dropna(axis=0)
+        return unit_df
 
-
-def read_combined_data(verbose=True):
-    try:
-        if verbose:
-            print('Reading "combined_data.csv"')
-        df = pd.read_csv(os.path.join(os.getcwd(), c.DATA_PATH,
-                                      'combined_data.csv'),
-                         usecols=c.FEATURES.append('UNIT'),
-                         index_col=False)
-        if verbose:
-            print('Processing data')
-        df[c.FEATURES_NO_TIME] = df[c.FEATURES_NO_TIME].apply(pd.to_numeric,
+    def read_combined_data(self, verbose=True):
+        try:
+            if verbose:
+                print('Reading "combined_data.csv"')
+            df = pd.read_csv(os.path.join(os.getcwd(), DATA_PATH, 'processed',
+                                          'combined_data.csv'),
+                             usecols=FEATURES.append('UNIT'),
+                             index_col=False)
+            if verbose:
+                print('Processing data')
+            df[FEATURES_NO_TIME] = df[FEATURES_NO_TIME].apply(pd.to_numeric,
                                                               errors='coerce')
-        df[c.FEATURES_NO_TIME] = df[c.FEATURES_NO_TIME].astype(np.float32)
-        df[['STEP', 'UNIT', 'TEST']] = df[['STEP', 'UNIT', 'TEST']].astype(
-            np.uint8,
-            errors='ignore',
-        )
-        df = df.dropna(axis=0)
-        return df
-    except:
-        print('No "combined_data.csv" found in the "data" directory')
-        return None
+            df[FEATURES_NO_TIME] = df[FEATURES_NO_TIME].astype(np.float32)
+            df[['STEP', 'UNIT', 'TEST']] = df[['STEP', 'UNIT', 'TEST']].astype(
+                np.uint8,
+                errors='ignore',
+            )
+            df = df.dropna(axis=0)
+            if verbose:
+                print('Done')
+            return df
+        except:
+            print('No "combined_data.csv" found in the "data" directory')
+            return None
 
+    def read_summary_file(self, verbose=True):
+        try:
+            xl = pd.ExcelFile(
+                os.path.join(DATA_PATH, 'processed',
+                             'report template-V4.xlsx'))
+            units = {}
+            for sheet in xl.sheet_names:
+                if 'HYD' in sheet:
+                    if verbose:
+                        print(f'Reading {sheet}')
+                    units[f'{sheet}'] = pd.read_excel(xl, sheet_name=sheet)
+            if verbose:
+                print(f'Done')
+            return units
+        except:
+            print('No "report template-V4.xlsx" found')
+            return None
 
-def read_summary_file():
-    try:
-        xl = pd.ExcelFile('report template-V4.xlsx')
-        units = {}
-        for sheet in xl.sheet_names:
-            if 'HYD' in sheet:
-                units[f'{sheet}'] = pd.read_excel(xl, sheet_name=sheet)
-        return units
-    except:
-        print('No "report template-V4.xlsx" found')
-        return None
-
-
-def load_data(read_all=True, raw=False, unit=None, verbose=True):
-    if read_all:
-        if raw:
-            return read_all_raw_data(verbose=verbose)
+    def load_data(self, read_all=True, raw=False, unit=None, verbose=True):
+        if read_all:
+            if raw:
+                return self.read_all_raw_data(verbose=verbose)
+            else:
+                return self.read_combined_data(verbose=verbose)
         else:
-            return read_combined_data(verbose=verbose)
-    else:
-        if raw:
-            return read_raw_unit_data(unit_id=unit)
-        else:
-            return pd.DataFrame(read_summary_file())
+            if raw:
+                return self.read_raw_unit_data(unit_id=unit)
+            else:
+                return pd.DataFrame(self.read_summary_file())
 
 
-def remove_outliers(df, z_score):
-    return df[(np.abs(stats.zscore(df[c.FEATURES_NO_TIME])) < z_score).all(
-        axis=1)]
+class ModelReader:
+    pass
 
 
-def remove_step_zero(df):
-    return df.drop(df[df['STEP'] == 0].index, axis=0)
+class Preprocessor:
+    def remove_outliers(self, df, zscore=3):
+        from scipy import stats
+        return df[(np.abs(stats.zscore(df[FEATURES_NO_TIME])) < zscore).all(
+            axis=1)]
+
+    def remove_step_zero(self, df):
+        return df.drop(df[df['STEP'] == 0].index, axis=0)
+
+    def get_warm_up_steps(self, df):
+        return df[(df['STEP'] >= 1) & (df['STEP'] <= 11)]
+
+    def get_break_in_steps(self, df):
+        return df[(df['STEP'] >= 12) & (df['STEP'] <= 22)]
+
+    def get_performance_check_steps(self, df):
+        return df[(df['STEP'] >= 23) & (df['STEP'] <= 33)]
 
 
-def get_warm_up_steps(df):
-    return df[(df['STEP'] >= 1) & (df['STEP'] <= 11)]
-
-
-def get_break_in_steps(df):
-    return df[(df['STEP'] >= 12) & (df['STEP'] <= 22)]
-
-
-def get_performance_check_steps(df):
-    return df[(df['STEP'] >= 23) & (df['STEP'] <= 33)]
+if __name__ == '__main__':
+    os.chdir('../../..')
+    os.getcwd()
+    data_reader = DataReader()
+    all_raw_data = data_reader.read_all_raw_data(verbose=True)
+    all_combined_data = data_reader.read_combined_data(verbose=True)
+    summary = data_reader.read_summary_file(verbose=True)
+    combined_data = data_reader.load_data(raw=False)
+    preprocessor = Preprocessor()
+    wo_outliers = preprocessor.remove_outliers(combined_data, zscore=3)
+    wo_step_zero = preprocessor.remove_step_zero(combined_data)
+    warm_up = preprocessor.get_warm_up_steps(combined_data)
+    break_in = preprocessor.get_break_in_steps(combined_data)
+    performance_check = preprocessor.get_performance_check_steps(combined_data)
