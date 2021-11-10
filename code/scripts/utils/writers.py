03a3fe2f9a7de3ce49eec648ac676d0ade5f462c
Ivan <ivan.okhotnikov@outlook.com>
2021-11-10 16:32:09 +0000

    Save model work started

--- code/scripts/utils/writers.py
+++ code/scripts/utils/writers.py
@@ -1,8 +1,14 @@
-from .config import MODELS_PATH
-
+from config import MODELS_PATH
+from joblib import dump, load
 
 def save_data(df):
     """
     Save dataframe to csv file
     """
     df.to_csv(MODELS_PATH + 'data.csv', index=False)
+
+def save_model(model):
+    """
+    Save model to pickle file
+    """
+    dump(model, MODELS_PATH + 'model.joblib')
