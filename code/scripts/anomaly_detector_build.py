47d701d4c0bd57f91d44349a08e48a6820b30bf3
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-11-11 19:17:10 +0000

    Plotting regular units and training/loading predictions option added

--- code/scripts/anomaly_detector_build.py
+++ code/scripts/anomaly_detector_build.py
@@ -16,16 +16,22 @@ if __name__ == '__main__':
     print(os.getcwd())
     df = DataReader.load_data(raw=True)
     df = Preprocessor.remove_step_zero(df)
-    iforest = IsolationForest(n_estimators=10000,
-                              contamination=0.01,
-                              bootstrap=True,
-                              warm_start=True,
-                              random_state=SEED,
-                              verbose=VERBOSITY,
-                              n_jobs=-1)
-    iforest_predict = iforest.fit_predict(df[FEATURES_NO_TIME_AND_COMMANDS])
-    dump(iforest, os.path.join(MODELS_PATH, 'iforest.joblib'))
-    df['ANOMALY'] = pd.Series(iforest_predict).astype(np.int8)
+    train = False
+    if train:
+        iforest = IsolationForest(n_estimators=10000,
+                                  contamination=0.01,
+                                  bootstrap=True,
+                                  warm_start=True,
+                                  random_state=SEED,
+                                  verbose=VERBOSITY,
+                                  n_jobs=-1)
+        iforest_predict = iforest.fit_predict(
+            df[FEATURES_NO_TIME_AND_COMMANDS])
+        dump(iforest, os.path.join(MODELS_PATH, 'iforest.joblib'))
+        df['ANOMALY'] = pd.Series(iforest_predict).astype(np.int8)
+    else:
+        df['ANOMALY'] = pd.read_csv(
+            os.path.join('outputs', 'predictions', 'iforest.csv'))
     anomalous_units = df[df['ANOMALY'] == -1]['UNIT'].unique()
     normal_units = [
         unit for unit in df['UNIT'].unique() if unit not in anomalous_units
@@ -34,3 +40,6 @@ if __name__ == '__main__':
     most_anomalous_units = anomlaies_per_unit.index[:5]
     for unit in most_anomalous_units:
         Plotter.plot_anomalies_per_unit_feature(df, unit=unit, feature='PT4')
+    Plotter.plot_unit_per_feature(df,
+                                  unit=random.choice(normal_units),
+                                  feature='PT4')
