33d824935419e3942d5834bd65af148300d1640b
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-12-02 13:22:25 +0400

    Minor updates

--- code/scripts/anomaly_detector.py
+++ code/scripts/anomaly_detector.py
@@ -4,17 +4,18 @@ from utils.readers import DataReader, Preprocessor, ModelReader
 from utils.plotters import Plotter
 from utils.config import FEATURES_NO_TIME, FEATURES_NO_TIME_AND_COMMANDS
 
-uploaded_file = st.file_uploader('Upload your data file', type=['csv', 'txt'])
+uploaded_file = st.file_uploader('Upload your data file', type=['csv'])
 if uploaded_file is not None:
     uploaded_df = DataReader.read_newcoming_data(uploaded_file)
-    st.write('Uploaded Data', uploaded_df)
-    preprocessed_df = Preprocessor.remove_step_zero(uploaded_df)
-    st.write('Preprocessed Data', preprocessed_df)
-    for feature in FEATURES_NO_TIME_AND_COMMANDS:
-        st.plotly_chart(
-            Plotter.plot_unit_per_test_feature(
-                preprocessed_df,
-                unit=uploaded_df.iloc[0]['UNIT'],
-                feature=feature,
-                save=False,
-                show=False))
+    if st.button('Show data'):
+        st.write('Uploaded Data', uploaded_df)
+        preprocessed_df = Preprocessor.remove_step_zero(uploaded_df)
+        st.write('Preprocessed Data', preprocessed_df)
+        for feature in FEATURES_NO_TIME:
+            st.plotly_chart(
+                Plotter.plot_unit_per_test_feature(
+                    preprocessed_df,
+                    unit=preprocessed_df.iloc[0]['UNIT'],
+                    feature=feature,
+                    save=False,
+                    show=False))
