667c4e6d1c52fee478deb2d5891b2b056064821b
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-12-03 13:41:21 +0400

    Refactored

--- code/scripts/anomaly_detector_build.py
+++ code/scripts/anomaly_detector_build.py
@@ -13,69 +13,79 @@ from utils.config import MODELS_PATH, PREDICTIONS_PATH, SEED, VERBOSITY, FEATURE
 from utils.readers import DataReader, Preprocessor
 from utils.plotters import Plotter
 
+
+def get_preprocessed_data():
+    df = DataReader.load_data(raw=False)
+    df = Preprocessor.remove_step_zero(df)
+    return df
+
+
+def get_model(model):
+    if model == 'IsolationForest':
+        detector = IsolationForest(n_estimators=10000,
+                                   contamination=0.01,
+                                   bootstrap=True,
+                                   warm_start=True,
+                                   random_state=SEED,
+                                   verbose=VERBOSITY,
+                                   n_jobs=-1)
+    elif model == 'DBSCAN':
+        detector = DBSCAN(eps=0.1,
+                          min_samples=10,
+                          metric='euclidean',
+                          algorithm='auto',
+                          leaf_size=30,
+                          p=None,
+                          n_jobs=-1)
+    elif model == 'LocalOutlierFactor':
+        detector = LocalOutlierFactor(n_neighbors=20,
+                                      algorithm='auto',
+                                      leaf_size=30,
+                                      metric='minkowski',
+                                      p=2,
+                                      contamination=0.01,
+                                      novelty=False,
+                                      n_jobs=-1)
+    return detector
+
+
+def save_model_and_its_predictions(df, model, detector, detector_predict):
+    print(f'Saving the model')
+    dump(
+        detector,
+        os.path.join(MODELS_PATH,
+                     f'{model}_{datetime.datetime.now():%d%m_%H%M}.joblib'))
+    print(f'Model saving finished')
+    print('Saving predictions')
+    pd.Series(detector_predict).astype(np.int8).to_csv(os.path.join(
+        PREDICTIONS_PATH, f'{model}_{datetime.datetime.now():%d%m_%H%M}.csv'),
+                                                       index=False)
+    print('Saving predictions finished')
+
+
+def fit_predict_model(df, model, detector):
+    print(f'Fitting {model} started')
+    detector_predict = detector.fit_predict(df[FEATURES_NO_TIME_AND_COMMANDS])
+    print(f'Fitting finished')
+    return detector_predict
+
+
 if __name__ == '__main__':
     print(os.getcwd())
     os.chdir('..\\..')
     print(os.getcwd())
-    df = DataReader.load_data(raw=False)
-    df = Preprocessor.remove_step_zero(df)
-    train = True
-    models = [
-        'IsolationForest', 'DBSCAN', 'LocalOutlierFactor']
-    model = models[0]
-    if train:
-        if model == 'IsolationForest':
-            detector = IsolationForest(n_estimators=10000,
-                                       contamination=0.01,
-                                       bootstrap=True,
-                                       warm_start=True,
-                                       random_state=SEED,
-                                       verbose=VERBOSITY,
-                                       n_jobs=-1)
-        elif model == 'DBSCAN':
-            detector = DBSCAN(eps=0.1,
-                              min_samples=10,
-                              metric='euclidean',
-                              algorithm='auto',
-                              leaf_size=30,
-                              p=None,
-                              n_jobs=-1)
-        elif model == 'LocalOutlierFactor':
-            detector = LocalOutlierFactor(n_neighbors=20,
-                                          algorithm='auto',
-                                          leaf_size=30,
-                                          metric='minkowski',
-                                          p=2,
-                                          contamination=0.01,
-                                          novelty=False,
-                                          n_jobs=-1)
-        print(f'Fitting {model} started')
-        detector_predict = detector.fit_predict(
-            df[FEATURES_NO_TIME_AND_COMMANDS])
-        print(f'Fitting finished')
-        print(f'Saving the model')
-        dump(
-            detector,
-            os.path.join(
-                MODELS_PATH,
-                f'{model}_{datetime.datetime.now():%d%m_%H%M}.joblib'))
-        print(f'Model saving finished')
-        df['ANOMALY'] = pd.Series(detector_predict).astype(np.int8)
-        print('Saving predictions')
-        df['ANOMALY'].to_csv(os.path.join(
-            PREDICTIONS_PATH,
-            f'{model}_{datetime.datetime.now():%d%m_%H%M}.csv'),
-                             index=False)
-        print('Saving predictions finished')
-    else:
-        df['ANOMALY'] = pd.read_csv(
-            os.path.join('outputs', 'predictions', f'iforest_2911_2247.csv'))
+    df = get_preprocessed_data()
+    model = 'IsolationForest'
+    detector = get_model(model)
+    detector_predict = fit_predict_model(df, model, detector)
+    df['ANOMALY'] = pd.Series(detector_predict).astype(np.int8)
+    save_model_and_its_predictions(df, model, detector, detector_predict)
     anomalous_units = df[df['ANOMALY'] == -1]['UNIT'].unique()
     normal_units = [
         unit for unit in df['UNIT'].unique() if unit not in anomalous_units
     ]
-    anomalies_per_unit = df[df['ANOMALY'] == -1]['UNIT'].value_counts()
-    most_anomalous_units = anomalies_per_unit.index[:5]
+    most_anomalous_units = df[df['ANOMALY'] ==
+                              -1]['UNIT'].value_counts().index[:5]
     for unit in most_anomalous_units:
         Plotter.plot_anomalies_per_unit_feature(df,
                                                 unit=unit,
