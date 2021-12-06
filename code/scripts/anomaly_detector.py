83db85fb160f3668c41b816c10bcbc2da63fa903
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-12-07 00:47:30 +0400

    Minor update

--- code/scripts/anomaly_detector.py
+++ code/scripts/anomaly_detector.py
@@ -4,7 +4,6 @@ from utils.readers import DataReader, Preprocessor, ModelReader
 from utils.plotters import Plotter
 from utils.config import FEATURES_NO_TIME, FEATURES_NO_TIME_AND_COMMANDS
 
-
 uploaded_file = st.file_uploader('Upload your data file', type=['csv'])
 if uploaded_file is not None:
     uploaded_df = DataReader.read_newcoming_data(uploaded_file)
@@ -16,16 +15,16 @@ if uploaded_file is not None:
         for feature in FEATURES_NO_TIME:
             st.plotly_chart(
                 Plotter.plot_unit_per_test_feature(df,
-                                                    unit=df.iloc[0]['UNIT'],
-                                                    feature=feature,
-                                                    save=False,
-                                                    show=False))
+                                                   unit=df.iloc[0]['UNIT'],
+                                                   feature=feature,
+                                                   save=False,
+                                                   show=False))
 if uploaded_file is not None:
     if st.button('Predict anomaly'):
         for feature in FEATURES_NO_TIME_AND_COMMANDS:
             detector = ModelReader.read_model(f'IsolationForest_{feature}')
             df[f'ANOMALY_{feature}'] = detector.predict(
-                Preprocessor.reshape_df(df[feature]))
+                df[feature].values.reshape(-1, 1))
             try:
                 st.plotly_chart(
                     Plotter.plot_anomalies_per_unit_test_feature(
