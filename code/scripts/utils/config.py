f90bf038f07b2c991857129876f2ca797d1eeaf5
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-12-06 23:15:37 +0400

    Reformatted

--- code/scripts/utils/config.py
+++ code/scripts/utils/config.py
@@ -7,40 +7,12 @@ PREDICTIONS_PATH = os.path.join(PROJECT_DIR, 'outputs', 'predictions')
 DATA_PATH = os.path.join(PROJECT_DIR, 'data')
 
 FEATURES = [
-    'TIME',
-    'STEP',
-    'HSU DEMAND',
-    'M1 SPEED',
-    'M1 CURRENT',
-    'M1 TORQUE',
-    'PT4 SETPOINT',
-    'PT4',
-    'D1 RPM',
-    'D1 CURRENT',
-    'D1 TORQUE',
-    'M2 RPM',
-    'M2 Amp',
-    'M2 Torque',
-    'CHARGE PT',
-    'CHARGE FLOW',
-    'M3 RPM',
-    'M3 Amp',
-    'M3 Torque',
-    'Servo PT',
-    'SERVO FLOW',
-    'M4 ANGLE',
-    'HSU IN',
-    'TT2',
-    'HSU OUT',
-    'M5 RPM',
-    'M5 Amp',
-    'M5 Torque',
-    'M6 RPM',
-    'M6 Amp',
-    'M6 Torque',
-    'M7 RPM',
-    'M7 Amp',
-    'M7 Torque',
+    'TIME', 'STEP', 'HSU DEMAND', 'M1 SPEED', 'M1 CURRENT', 'M1 TORQUE',
+    'PT4 SETPOINT', 'PT4', 'D1 RPM', 'D1 CURRENT', 'D1 TORQUE', 'M2 RPM',
+    'M2 Amp', 'M2 Torque', 'CHARGE PT', 'CHARGE FLOW', 'M3 RPM', 'M3 Amp',
+    'M3 Torque', 'Servo PT', 'SERVO FLOW', 'M4 ANGLE', 'HSU IN', 'TT2',
+    'HSU OUT', 'M5 RPM', 'M5 Amp', 'M5 Torque', 'M6 RPM', 'M6 Amp',
+    'M6 Torque', 'M7 RPM', 'M7 Amp', 'M7 Torque'
 ]
 FEATURES_NO_TIME = [
     # 'TIME',
@@ -76,7 +48,7 @@ FEATURES_NO_TIME = [
     'M6 Torque',
     'M7 RPM',
     'M7 Amp',
-    'M7 Torque',
+    'M7 Torque'
 ]
 FEATURES_NO_TIME_AND_COMMANDS = [
     # 'TIME',
@@ -112,20 +84,11 @@ FEATURES_NO_TIME_AND_COMMANDS = [
     'M6 Torque',
     'M7 RPM',
     'M7 Amp',
-    'M7 Torque',
+    'M7 Torque'
 ]
 TRIAL_DRIFT = [
-    'STEP',
-    'UNIT',
-    'TEST',
-    'M3 Amp',
-    'M3 Torque',
-    'Servo PT',
-    'HSU IN',
-    'TT2',
-    'HSU OUT',
-    'M7 Amp',
-    'M7 Torque',
+    'STEP', 'UNIT', 'TEST', 'M3 Amp', 'M3 Torque', 'Servo PT', 'HSU IN', 'TT2',
+    'HSU OUT', 'M7 Amp', 'M7 Torque'
 ]
 
 FOLDS = 5
