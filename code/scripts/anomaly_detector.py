cd73131bf105b6cc9b97d8eef5a78da6daa775ba
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2021-12-06 23:15:20 +0400

    Plotting anomalies predictions found for each feature added

--- code/scripts/anomaly_detector.py
+++ code/scripts/anomaly_detector.py
@@ -4,6 +4,7 @@ from utils.readers import DataReader, Preprocessor, ModelReader
 from utils.plotters import Plotter
 from utils.config import FEATURES_NO_TIME, FEATURES_NO_TIME_AND_COMMANDS
 
+
 uploaded_file = st.file_uploader('Upload your data file', type=['csv'])
 if uploaded_file is not None:
     uploaded_df = DataReader.read_newcoming_data(uploaded_file)
@@ -11,24 +12,30 @@ if uploaded_file is not None:
     if st.button('Show data'):
         st.write('Uploaded Data', uploaded_df)
         st.write('Preprocessed Data', df)
-    for feature in FEATURES_NO_TIME:
-        st.plotly_chart(
-            Plotter.plot_unit_per_test_feature(df,
-                                               unit=df.iloc[0]['UNIT'],
-                                               feature=feature,
-                                               save=False,
-                                               show=False))
-    detector = ModelReader.read_model('IsolationForest_0212_1742')
-    df['ANOMALY'] = detector.predict(df[FEATURES_NO_TIME_AND_COMMANDS])
-    for feature in FEATURES_NO_TIME_AND_COMMANDS:
-        try:
+    if st.button('Plot data'):
+        for feature in FEATURES_NO_TIME:
             st.plotly_chart(
-                Plotter.plot_anomalies_per_unit_test_feature(
-                    df,
-                    unit=df.iloc[0]['UNIT'],
-                    test=1,
-                    feature=feature,
-                    show=False))
-        except:
-            st.write(f'No anomalies found in {df.iloc[0]["UNIT"]} for {feature}')
-            continue
+                Plotter.plot_unit_per_test_feature(df,
+                                                    unit=df.iloc[0]['UNIT'],
+                                                    feature=feature,
+                                                    save=False,
+                                                    show=False))
+if uploaded_file is not None:
+    if st.button('Predict anomaly'):
+        for feature in FEATURES_NO_TIME_AND_COMMANDS:
+            detector = ModelReader.read_model(f'IsolationForest_{feature}')
+            df[f'ANOMALY_{feature}'] = detector.predict(
+                Preprocessor.reshape_df(df[feature]))
+            try:
+                st.plotly_chart(
+                    Plotter.plot_anomalies_per_unit_test_feature(
+                        df,
+                        unit=df.iloc[0]['UNIT'],
+                        test=1,
+                        feature=feature,
+                        show=False))
+            except:
+                st.write(
+                    f'No anomalies found in {df.iloc[0]["UNIT"]} for {feature}'
+                )
+                continue
