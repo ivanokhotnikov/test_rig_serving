fab7116f157406d912317ed3c33feb3d0523ebbf
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-01-06 22:13:42 +0000

    stl decomposition tested

--- code/scripts/timeseries_analysis.py
+++ code/scripts/timeseries_analysis.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.seasonal import seasonal_decompose, STL
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
 
 from utils.config import FEATURES, FEATURES_NO_TIME_AND_COMMANDS
@@ -19,26 +19,37 @@ if __name__ == '__main__':
                                        freq='S')
     df.set_index('RUNNING TIME')
     plot_all = False
-    plot_only_trend = True
+    plot_only_trends = True
+    model = 'stl'
+    decompositions = {feature: [] for feature in FEATURES_NO_TIME_AND_COMMANDS}
     for feature in FEATURES_NO_TIME_AND_COMMANDS:
-        additive_decomposition = seasonal_decompose(
-            df[feature],
-            model='additive',
-            period=5400,
-        )
+        if model == 'seasonal_decompose':
+            decomposition = seasonal_decompose(
+                df[feature],
+                model='additive',
+                period=5400,
+            )
+        if model == 'stl':
+            decomposition = STL(
+                df[feature],
+                period=5400,
+                seasonal_jump=1000,
+                trend_jump=1000,
+            ).fit()
+        decompositions[feature] = decomposition
         if plot_all:
             for plot in ('observed', 'trend', 'seasonal', 'resid'):
                 fig = go.Figure(
                     go.Scatter(x=df['RUNNING TIME'],
-                               y=getattr(additive_decomposition, plot)))
+                               y=getattr(decomposition, plot)))
                 fig.update_layout(title=f'{plot.capitalize()} {feature}',
                                   xaxis_title='Running time',
                                   template='none')
                 fig.show()
-        if plot_only_trend:
+        if plot_only_trends:
             fig = go.Figure(
                 go.Scatter(x=df['RUNNING TIME'],
-                           y=getattr(additive_decomposition, 'trend')))
+                           y=getattr(decomposition, 'trend')))
             fig.update_layout(title=f'Trend {feature}',
                               xaxis_title='Running time',
                               template='none')
