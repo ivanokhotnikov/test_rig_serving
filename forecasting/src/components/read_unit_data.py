bf11f1070fb77aad03c749095b02ed9887d65c4f
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-10-31 12:39:52 +0000

    reading unit data relaxed

--- forecasting/src/components/read_unit_data.py
+++ forecasting/src/components/read_unit_data.py
@@ -12,12 +12,9 @@ def read_unit_data(file_name):
     data_blob = RAW_DATA_BUCKET.get_blob(file_name)
     df = pd.read_csv(
         io.BytesIO(data_blob.download_as_bytes()),
-        usecols=RAW_FORECAST_FEATURES,
         index_col=False,
+        header=0,
     )
-    df[FEATURES_NO_TIME] = df[FEATURES_NO_TIME].apply(pd.to_numeric,
-                                                      errors='coerce',
-                                                      downcast='float')
     df = df.dropna(axis=0)
     try:
         unit = int(re.split(r'_|-|/', file_name)[0][-4:].lstrip('/HYDhyd0'))
@@ -25,7 +22,4 @@ def read_unit_data(file_name):
     except ValueError as err:
         logging.info(f'{err}\n. Cannot parse unit from {file_name}')
         df['UNIT'] = pd.NA
-    df['STEP'] = df['STEP'].astype(int)
-    #TODO retesting case needs to be addressed
-    df['TEST'] = int(1)
     return df
